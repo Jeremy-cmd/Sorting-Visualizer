6e146335ab5d3f985c0cd5d8296fdc5094d16b31
Jeremy-cmd <60899184+Jeremy-cmd@users.noreply.github.com>
2022-04-06 16:04:47 -0400

    fixed bubble sort animation bug

--- src/Sorting.jsx
+++ src/Sorting.jsx
@@ -31,9 +31,11 @@ function Sorting() {
 
   }
 
-  function merge() {
-    const animations = getMergeAnimation(sortArray);
+  function toSort(animate) {
+    const animations = animate;
+
     console.log(animations);
+    console.log("the sorted array is " + sortArray);
     for(let i=0; i<animations.length; i++){
       const bars = document.getElementsByClassName('bar');
       // divided by three, every three sections one swap
@@ -51,7 +53,13 @@ function Sorting() {
         // third item in animations means the swapping
         setTimeout(() => {
           const [firstBarIdx, height] = animations[i];
+          let cur = (parseInt(firstBarIdx)+1)
+
+          if(cur < sortArray.length && parseInt(bars[firstBarIdx].style.height, 10) > parseInt(bars[firstBarIdx+1].style.height, 10)){
+            bars[firstBarIdx+1].style.height = bars[firstBarIdx].style.height;
+          }
           bars[firstBarIdx].style.height = `${height}px`;
+
         }, i * ANIMATION_SPEED_MS);
       }
     }
@@ -59,31 +67,15 @@ function Sorting() {
 
   }
 
+  function merge() {
+    const animations = getMergeAnimation(sortArray);
+    toSort(animations);
+  }
+
   function bubble() {
-
+    console.log("the array is " + sortArray);
     const animations = getBubbleAnimation(sortArray);
-    console.log(animations);
-    for(let i=0; i<animations.length; i++){
-      const bars = document.getElementsByClassName("bar");
-      const changeColor = i % 3 !== 2;
-      if(changeColor){
-        const [firstBarIdx, secondBarIdx] = animations[i];
-        const color = i % 3 === 0 ? SECONDARY_COLOR : COLOR;
-        setTimeout(() => {
-          bars[firstBarIdx].style.backgroundColor = color;
-          bars[secondBarIdx].style.backgroundColor = color;
-        }, i * ANIMATION_SPEED_MS);
-      }
-      else{
-        setTimeout(() => {
-          const [firstBarIdx, height] = animations[i];
-          bars[firstBarIdx].style.height = `${height}px`;
-        }, i * ANIMATION_SPEED_MS);
-
-      }
-
-    }
-
+    toSort(animations);
   }
 
   function heap() {
